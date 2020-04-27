446e0b1f09b9110b8b0330461c62aa9b59d49b80
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-04-27 23:29:51 +0530

    Day 16 - Created Project 1 in Xcode /w Swift and UIKit

---
+++ Project 1/Project 1/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Shreyas on 27/04/20.
+//  Copyright © 2020 Shreyas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contents(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(contentsOf: item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
+
